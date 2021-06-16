0fafa57bdd10da1f4cbb9fa5cd0da82c5800f52e
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-16 09:45:22 -0300

    Projeto Iniciado

---
+++ ExceptionsEX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionsEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
